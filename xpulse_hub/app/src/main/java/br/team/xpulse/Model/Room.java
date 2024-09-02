9339abf22d57e63482c901d98096590c31f8c167
DevDuque <devgduque@gmail.com>
2024-09-02 15:56:26 -0300

    Edita a classe de Salas

--- xpulse_hub/app/src/main/java/br/team/xpulse/Model/Room.java
+++ xpulse_hub/app/src/main/java/br/team/xpulse/Model/Room.java
@@ -1,16 +1,69 @@
 package br.team.xpulse.Model;
 
+import java.util.Date;
+
 public class Room {
 
     private String name;
-    private String activity;
-    private Integer maxPlayers;
+    private String category;
+    private String server;
+    private Date dateTime;
+    private Activity activity;
     private String description;
 
-    public Room(String name, String activity, Integer maxPlayers, String description) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+    public void setActivity(Activity activity) {
         this.activity = activity;
-        this.maxPlayers = maxPlayers;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Room(String name, String category, String server, Activity activity, String description) {
+        this.name = name;
+        this.category = category;
+        this.server = server;
+        this.dateTime = new Date();
+        this.activity = activity;
         this.description = description;
     }
 }
